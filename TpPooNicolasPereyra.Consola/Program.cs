4c0c9e41bde9400b176b5a5a356c7d7cabd22beb
Nico Pereyr <nicolaspereyra33@gmail.com>
2025-05-02 21:43:43 -0300

    Agregar archivos de proyecto.

---
+++ TpPooNicolasPereyra.Consola/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using TpPooNicolasPereyra.Entidades;
+using Utilidades;
+
+namespace TpPooNicolasPereyra.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Piramides");
+
+            var lado = ExtensionesConsola.PedirEntero("Ingrese el valor del lado de la base de la pirámide:");
+            var altura = ExtensionesConsola.PedirEntero("Ingrese el valor de la altura de la pirámide:");
+
+            PiramideCuadrada r = new PiramideCuadrada(lado, altura);
+
+            var context = new ValidationContext(r);
+            var errors = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(r, context, errors, true);
+            if (isValid)
+            {
+                Console.WriteLine(r);
+            }
+            else
+            {
+                foreach (var error in errors)
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+            }
+        }
+    }
+}
---
+++ TpPooNicolasPereyra.Entidades/PiramideCuadrada.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Text;
+
+namespace TpPooNicolasPereyra.Entidades
+{
+    public class PiramideCuadrada
+    {
+        private int lado;
+        private int altura;
+        private double apotema;
+
+
+        public PiramideCuadrada(int lado, int altura)
+        {
+            this.lado = lado;
+            this.altura = altura;
+            CalcularApotema();
+        }
+
+        private void CalcularApotema()
+        {
+            apotema = Math.Sqrt(Math.Pow(lado / 2.0, 2) + Math.Pow(altura, 2));
+        }
+
+        public double ObtenerApotema()
+        {
+            return apotema;
+        }
+
+        public double CalcularAreaBase()
+        {
+            return Math.Pow(lado, 2);
+        }
+        public double CalcularAreaLateral()
+        {
+            return 2 * lado * apotema;
+        }
+        public double CalcularAreaTotal()
+        {
+            return CalcularAreaBase() + CalcularAreaLateral();
+        }
+        public double CalcularVolumen()
+        {
+            return (Math.Pow(lado, 2) * altura) / 3.0;
+        }
+        public int Lado { get => lado;}
+        public int Altura { get => altura;}
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Lado de la base: {lado}");
+            sb.AppendLine($"Altura.............:{altura}");
+            sb.AppendLine($"Volumen............:{CalcularVolumen():F2}");
+            sb.AppendLine($"Area.............:{CalcularAreaTotal():F2}");
+            sb.AppendLine($"Apotema..............:{apotema}");
+            return sb.ToString();
+        }
+    }
+
+}
